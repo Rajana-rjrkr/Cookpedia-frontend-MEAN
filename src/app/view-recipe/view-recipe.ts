75db3e677058062a61b766df3d1718a640670fb7
Rajana-rjrkr <rajanaramkumar676@gmail.com>
2025-11-22 18:41:25 +0530

    View designed

--- src/app/view-recipe/view-recipe.ts
+++ src/app/view-recipe/view-recipe.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-view-recipe',
-  imports: [],
+  imports: [Header, Footer, RouterLink],
   templateUrl: './view-recipe.html',
   styleUrl: './view-recipe.css',
 })
